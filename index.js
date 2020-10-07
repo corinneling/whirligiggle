d1c7df99a2902af29a643b7b4243a6aae72184a1
corinne <corinne@heysparkbox.com>
2020-10-07 17:19:52 -0400

    feat: finish adding unit tests

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-import { lazyLoadImages } from './src/index';
+import { lazyLoadImages } from './src/lazy-load';
 import { addMarkupToPage } from './src/markup';
 import { handleButtonNavigation, handleDotNavigation } from './src/navigation';
 
 export function init(elementId, data) {
-    addMarkupToPage(elementId, data)
-    if (data.showButtons) handleButtonNavigation();
-    if (data.showDots) handleDotNavigation();
-    lazyLoadImages();
+  addMarkupToPage(elementId, data)
+  if (data.showButtons) handleButtonNavigation();
+  if (data.showDots) handleDotNavigation();
+  lazyLoadImages();
 }
