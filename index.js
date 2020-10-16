75c27d0bf133b532ae7ec64b2fb9018cd14ed89c
corinne <corinne@heysparkbox.com>
2020-10-16 15:43:57 -0400

    add parcel

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import { lazyLoadImages } from './src/lazy-load';
 import { addMarkupToPage } from './src/markup';
 import { handleButtonNavigation, handleDotNavigation, setSlideLinkTabindex } from './src/navigation';
+import './src/scss/styles.scss';
 
 export function init(elementId, data) {
   addMarkupToPage(elementId, data)
