c8e67d31fdf4f261c0c4626dc525ae2b4686379e
corinne <corinne@heysparkbox.com>
2020-10-20 12:28:40 -0400

    setup parcel

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ import { addMarkupToPage } from './src/markup';
 import { handleButtonNavigation, handleDotNavigation, setSlideLinkTabindex } from './src/navigation';
 import './src/scss/styles.scss';
 
+if (typeof NodeList !== "undefined" && NodeList.prototype && !NodeList.prototype.forEach) {
+  NodeList.prototype.forEach = Array.prototype.forEach;
+}
+
+if (typeof NodeList !== "undefined" && NodeList.prototype && !NodeList.prototype.indexOf) {
+  NodeList.prototype.indexOf = Array.prototype.indexOf;
+}
+
 export function init(elementId, data) {
   addMarkupToPage(elementId, data)
   if (data.showButtons) handleButtonNavigation();
